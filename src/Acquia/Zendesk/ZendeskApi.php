70a8c72d56b37d0214450f861b510c23b7ef0935
Richard Burford <rich@freestylesystems.co.uk>
2014-02-03 05:29:16 +0000

    Added some methods for retrieving tickets.

--- src/Acquia/Zendesk/ZendeskApi.php
+++ src/Acquia/Zendesk/ZendeskApi.php
@@ -39,6 +39,134 @@ class ZendeskApi {
   }
 
   /**
+   * Gets a ticket by ticket ID.
+   *
+   * @param int $ticket_id
+   *   The ID of the ticket to retrieve.
+   *
+   * @return object
+   *   The ticket object with ticket values as direct object properties.
+   */
+  public function getTicket($ticket_id) {
+    $data = $this->request('GET', "tickets/${ticket_id}");
+    return $data->ticket;
+  }
+
+  /**
+   * Gets multiple tickets associated with a given list of ticket IDs.
+   *
+   * @param array $ticket_ids
+   *   An array of ticket IDs.
+   *
+   * @return object
+   *   An object of tickets.
+   */
+  public function getTickets($ticket_ids) {
+    $data = $this->request('GET', 'tickets/show_many', array(), NULL, array(
+      'query' => array('ids' => implode(',', $ticket_ids))
+    ));
+    return $data->tickets;
+  }
+
+  /**
+   * Gets all tickets in chronological order.
+   *
+   * As pointed out in the Zendesk API documentation, this method may not
+   * actually return all of the tickets due to ticket archiving.
+   *
+   * @return
+   *   The response object of the request containing all of the tickets.
+   */
+  public function getTicketsAll() {
+    return $this->request('GET', 'tickets');
+  }
+
+  /**
+   * Gets lists of tickets via the Zendesk Search API.
+   *
+   * @param string $search
+   *   The search text to be matched or a search string. See the Search
+   *   Reference: https://support.zendesk.com/entries/20239737
+   * @param string $sort_by
+   *   The possible values are "updated_at", "created_at", "priority", "status",
+   *   and "ticket_type".
+   * @param string $order
+   *   One of "relevance", "asc", or "desc". Defaults to "relevance" when no
+   *   criteria is specified.
+   *
+   * @return object
+   *   The response object of the request containing tickets matching the
+   *   search paramaters.
+   */
+  public function getTicketsSearch($search, $sort_by = NULL, $order = NULL) {
+    $query = array(
+      'query' => $search,
+      'sort_by' => $sort_by,
+      'sort_order' => $order,
+    );
+    return $this->request('GET', 'search', array(), NULL, array('query' => $query));
+  }
+
+  /**
+   * Gets tickets associated with a given organization ID.
+   *
+   * @param int $organization_id
+   *   The organization ID to get tickets for.
+   *
+   * @return object
+   *   The response object of the request containing tickets associated with the
+   *   given organization ID.
+   */
+  public function getTicketsOrganization($organization_id) {
+    return $this->request('GET', "organizations/${organization_id}/tickets");
+  }
+
+  /**
+   * Gets tickets associated with a given user ID.
+   *
+   * @param int $user_id
+   *   The user ID to retrieve tickets for.
+   * @param string $type
+   *   One of "requested" and "ccd".
+   *
+   * @return object
+   *   The response object of the request containing tickets associated with the
+   *   given user ID.
+   */
+  public function getTicketsUser($user_id, $type = 'requested') {
+    return $this->request('GET', "users/${user_id}/tickets/${type}");
+  }
+
+  /**
+   * Gets a short list of the most recent tickets.
+   *
+   * @return object
+   *   The response object containing the most recent tickets.
+   */
+  public function getTicketsRecent() {
+    return $this->request('GET', 'tickets/recent');
+  }
+
+  /**
+   * Gets lists of tickets via the Incremental Tickets API.
+   *
+   * The Incremental Tickets API is meant to be a lightweight, but heavily
+   * rate-limited endpoint for listing tickets. It provides a way to easily
+   * export tickets into an external system, and then poll for udpates.
+   *
+   * @param int $start_time
+   *   The Unix time for when the search results should start. Defaults to 1
+   *   year ago.
+   */
+  public function getTicketsIncremental($start_time = NULL) {
+    if (empty($start_time)) {
+      $start_time = time() - 86400 * 365;
+    }
+    return $this->request('GET', 'exports/tickets', array(), NULL, array(
+      'query' => array('start_time' => $start_time)));
+  }
+
+  /**
    * Makes an HTTP request to the API.
    *
    * @param string $method
